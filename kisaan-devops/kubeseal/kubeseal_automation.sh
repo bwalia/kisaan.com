391605f8b8c6945000b2886f2b24d253bec40790
GitLab Automation <automation@gitlab.local>
2025-09-26 23:37:09 +0100

    CI CD workflows added

--- kisaan-devops/kubeseal/kubeseal_automation.sh
+++ kisaan-devops/kubeseal/kubeseal_automation.sh
@@ -227,6 +227,8 @@ content = content.replace('helmfilesecretsplaceholder', '$SAFE_SEALEDSECRET_ENCR
 content = content.replace('CICD_NAMESPACE_PLACEHOLDER', '$CICD_NAMESPACE')
 content = content.replace('CICD_ENV_FILE_PLACEHOLDER_BASE64', '$SAFE_SEALEDSECRET_ENCRYPTED')
 content = content.replace('CICD_ENV_REF_PLACEHOLDER', '$ENV_REF')
+content = content.replace('prod.', '.')
+content = content.replace('prod-www.', 'www.')
 # Write back to file
 with open('$HELM_VALUES_OUTPUT_PATH', 'w') as f:
     f.write(content)
